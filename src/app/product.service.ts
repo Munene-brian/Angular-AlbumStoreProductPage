2f3fdea415a37cdb9b350f8823454b104569b39a
munene brian <52892865+Munene-brian@users.noreply.github.com>
2021-10-08 14:05:26 +0300

    interfaceupdate

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
@@ -8,8 +10,8 @@ export class ProductService {
 
    }
 
-  getAlbum(id:number) {
-    return this._http.get(this._albumUrl).map((response)=>
+  getAlbum(id:number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response)=><Album>
     response.json());
   }
 }
