3443daeb91f80bb4427813b7611e1d3a447e62ba
munene brian <52892865+Munene-brian@users.noreply.github.com>
2021-10-18 10:12:51 +0300

    Test 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
     ProductPageComponent,
     ProductDescriptionComponent,
     
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from 'app/product';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products:Product[]
+  constructor( private _productService:ProductService) { }
+
+  ngOnInit() {
+  }
+
+}
